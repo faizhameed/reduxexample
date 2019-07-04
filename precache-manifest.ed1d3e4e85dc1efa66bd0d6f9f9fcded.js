aa471d74bdd0d214e723ead13cf6ba1791121401
Faiz Hameed <faizftb@gmail.com>
2019-07-04 18:06:02 +0530

    Updates

---
+++ precache-manifest.ed1d3e4e85dc1efa66bd0d6f9f9fcded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5cafd74ce3be022f00a1d15f51ba10",
+    "url": "/reduxexample/index.html"
+  },
+  {
+    "revision": "94a0a84f7f0de6490978",
+    "url": "/reduxexample/static/css/main.49d8e0b5.chunk.css"
+  },
+  {
+    "revision": "9d7006a3932540d2a98f",
+    "url": "/reduxexample/static/js/2.c387a954.chunk.js"
+  },
+  {
+    "revision": "94a0a84f7f0de6490978",
+    "url": "/reduxexample/static/js/main.619b5d30.chunk.js"
+  },
+  {
+    "revision": "9300e342404956e001fc",
+    "url": "/reduxexample/static/js/runtime~main.0ef4c451.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reduxexample/static/media/logo.5d5d9eef.svg"
+  }
+]);
